937d2679102340f30e89945abae8c9f9d876e04d
KarimHazem186 <karimhazem186@gmail.com>
2025-06-26 23:08:48 +0300

    Update connect.js

--- server/db/connect.js
+++ server/db/connect.js
@@ -1,6 +1,5 @@
 const mongoose = require('mongoose')
 
-// const DB = 'mongodb+srv://mern-auth:mern1234@mern-auth.embzx.mongodb.net/ImgUpload?retryWrites=true&w=majority&appName=MERN-Auth'
 const DB = process.env.DATABASE 
 
 mongoose.connect(DB,{
